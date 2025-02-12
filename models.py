e2d7906e10468a748136afda07ee5b8e03bf1252
Suhendar Aditya <darhensu@gmail.com>
2025-02-12 18:41:48 +0700

    create database.py, models.py, schemas.py

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
 # model data
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
--- schemas.py
+++ schemas.py
@@ -1,1 +1,16 @@
 # skema data
+
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    name: str 
+    description: str 
+
+class ItemCreate(ItemBase):
+    pass
+
+class ItemRespnse(ItemBase):
+    id: int 
+
+    class Config:
+        orm_mode = True
