2fc3691d6825742e44d84705f2433f01a98075d2
Suhendar Aditya <darhensu@gmail.com>
2025-02-13 09:24:18 +0700

    create crud.py

--- crud.py
+++ crud.py
@@ -1,1 +1,33 @@
 # operasi CRUD
+
+from sqlalchemy.orm import Session
+import models, schemas
+
+def create_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(name=item.name, description=item.description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_items(db:Session):
+    return db.query(models.items).all()
+
+def get_item(db: Session, item_id: int):
+    return db.qury(models.Item).filter(models.Item.id == item_id).first()
+
+def update_item(db: Session, item_id: int, item: schemas.ItemCreate):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if db_item:
+        db_item.name = item.name
+        db_item.description = item.description
+        db.commit()
+        db.refresh(db_item)
+    return db_item
+
+def delete_item(db: Session, item_id: int):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+    return db_item
