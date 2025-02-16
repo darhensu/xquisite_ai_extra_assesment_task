a9b7f2f462bdc90a2a757333aa09fd6943c0763f
Suhendar Aditya <darhensu@gmail.com>
2025-02-16 09:20:32 +0700

    added category to schemas.py

--- schemas.py
+++ schemas.py
@@ -1,7 +1,25 @@
 # skema data
 
 from pydantic import BaseModel
+from typing import List, Optional
 
+# categories
+class CategoryBase(BaseModel):
+    name: str 
+    description: Optional[str] = None
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class CategoryResponse(CategoryBase):
+    id: int 
+    items: List["ItemResponse"] = [] # Relasi ke items
+
+    class Config:
+        from_attributes = True
+
+
+# items
 class ItemBase(BaseModel):
     name: str 
     description: str
